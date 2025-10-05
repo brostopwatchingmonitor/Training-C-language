6610ae5b2f6d0f1036aea011f37e8f67e4a9827e
AlifShidqi <alifshidqiputraamir@gmail.com>
2025-10-05 11:30:14 +0700

    perbaikan

--- Algoritma_PerhitunganLuasProgram_KolamRenang.cpp
+++ Algoritma_PerhitunganLuasProgram_KolamRenang.cpp
@@ -1,19 +1,23 @@
 #include <iostream>
+#include <iomanip>
 using namespace std;
 
 int main()
 {
+    // --- Konstanta ---
     const int P_Tanah = 100;
     const int L_Tanah = 40;
     const int P_Kolam = 12;
     const int L_Kolam = 5;
-    const int Biaya_bangun_per_meter = 1000000;
-    const int Biaya_rapikan_per_meter = 800000;
+    const long long Biaya_bangun_per_meter = 1000000;
+    const long long Biaya_rapikan_per_meter = 800000;
 
-    Luas_Tanah = P_Tanah * L_Tanah;
-    luas_kolam = P_Kolam * L_kolam;
-    luas_sisa-tanah = luas-tanah - luas-kolam;
-    biaya_kolam = luas_kolam * Biaya_bangun_per_meter;
-    biaya_sisa_lahan = luas_sisa-tanah * Biaya_rapikan_per_meter;
-    tatal-biaya = biaya_kolam + biaya_sisa_lahan;
+    // --- Deklarasi Variabel ---
+    // Menggunakan long long untuk menghindari overflow pada perhitungan biaya
+    long long luas_tanah;
+    long long luas_kolam;
+    long long luas_sisa_tanah;
+    long long biaya_kolam;
+    long long biaya_sisa_lahan;
+    long long total_biaya;
 }
