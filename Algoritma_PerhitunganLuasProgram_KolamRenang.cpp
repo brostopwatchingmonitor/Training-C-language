7ccd2f51471a506da8fc55b684c672f87aca94f8
AlifShidqi <alifshidqiputraamir@gmail.com>
2025-10-05 11:28:01 +0700

    task_campus2

--- Algoritma_PerhitunganLuasProgram_KolamRenang.cpp
+++ Algoritma_PerhitunganLuasProgram_KolamRenang.cpp
@@ -3,5 +3,17 @@ using namespace std;
 
 int main()
 {
+    const int P_Tanah = 100;
+    const int L_Tanah = 40;
+    const int P_Kolam = 12;
+    const int L_Kolam = 5;
+    const int Biaya_bangun_per_meter = 1000000;
+    const int Biaya_rapikan_per_meter = 800000;
 
+    Luas_Tanah = P_Tanah * L_Tanah;
+    luas_kolam = P_Kolam * L_kolam;
+    luas_sisa-tanah = luas-tanah - luas-kolam;
+    biaya_kolam = luas_kolam * Biaya_bangun_per_meter;
+    biaya_sisa_lahan = luas_sisa-tanah * Biaya_rapikan_per_meter;
+    tatal-biaya = biaya_kolam + biaya_sisa_lahan;
 }
