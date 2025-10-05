43a1964eef51386c143e9d27901ea33d3a47af9d
AlifShidqi <alifshidqiputraamir@gmail.com>
2025-10-05 11:31:37 +0700

    perhitungan Algoritma selesai

--- Algoritma_PerhitunganLuasProgram_KolamRenang.cpp
+++ Algoritma_PerhitunganLuasProgram_KolamRenang.cpp
@@ -20,4 +20,27 @@ int main()
     long long biaya_kolam;
     long long biaya_sisa_lahan;
     long long total_biaya;
+
+    // --- Perhitungan ---
+    luas_tanah = P_Tanah * L_Tanah;
+    luas_kolam = P_Kolam * L_Kolam;
+    luas_sisa_tanah = luas_tanah - luas_kolam;
+    biaya_kolam = luas_kolam * Biaya_bangun_per_meter;
+    biaya_sisa_lahan = luas_sisa_tanah * Biaya_rapikan_per_meter;
+    total_biaya = biaya_kolam + biaya_sisa_lahan;
+
+    // --- Menampilkan Hasil ---
+    std::cout << "--- Rincian Proyek Pembangunan ---" << std::endl;
+    std::cout << std::fixed << std::setprecision(0); // Mengatur format angka
+
+    std::cout << "Luas Tanah          : " << luas_tanah << " m2" << std::endl;
+    std::cout << "Luas Kolam          : " << luas_kolam << " m2" << std::endl;
+    std::cout << "Sisa Luas Lahan     : " << luas_sisa_tanah << " m2" << std::endl;
+    std::cout << "-------------------------------------" << std::endl;
+    std::cout << "Biaya Bangun Kolam  : Rp " << biaya_kolam << std::endl;
+    std::cout << "Biaya Rapikan Lahan : Rp " << biaya_sisa_lahan << std::endl;
+    std::cout << "-------------------------------------" << std::endl;
+    std::cout << "Total Biaya         : Rp " << total_biaya << std::endl;
+
+    return 0;
 }
